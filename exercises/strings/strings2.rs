ea0e520b620a6829533e223e4233079eca89ca93
xxbd <lisa20110815@163.com>
2024-10-16 10:57:38 +0800

    solve string4

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -7,7 +7,8 @@
 
 
 fn main() {
-    let word = String::from("green"); // Try not changing this line :)
+    let word = String::from("green");
+     // Try not changing this line :)
     if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
