6683d6314a2286c9c1ba392c01b75ea2a021618b
xxbd <lisa20110815@163.com>
2024-10-17 14:14:38 +0800

    solve using_as

--- exercises/conversions/using_as.rs
+++ exercises/conversions/using_as.rs
@@ -1,25 +1,18 @@
-// using_as.rs
-//
-// Type casting in Rust is done via the usage of the `as` operator. Please note
-// that the `as` operator is not only used when type casting. It also helps with
-// renaming imports.
-//
-// The goal is to make sure that the division does not fail to compile and
-// returns the proper type.
-//
-// Execute `rustlings hint using_as` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
-fn average(values: &[f64]) -> f64 {
-    let total = values.iter().sum::<f64>();
-    total / values.len()
+fn average(values: &[f64]) -> Option<f64> {
+    if values.is_empty() {
+        None
+    } else {
+        let total = values.iter().sum::<f64>();
+        Some(total / values.len() as f64) // Cast `usize` to `f64` to perform floating point division
+    }
 }
 
 fn main() {
     let values = [3.5, 0.3, 13.0, 11.7];
-    println!("{}", average(&values));
+    match average(&values) {
+        Some(avg) => println!("{}", avg),
+        None => println!("Cannot calculate the average of an empty list."),
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +21,11 @@ mod tests {
 
     #[test]
     fn returns_proper_type_and_value() {
-        assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]), 7.125);
+        assert_eq!(average(&[3.5, 0.3, 13.0, 11.7]), Some(7.125));
+    }
+
+    #[test]
+    fn returns_none_for_empty_slice() {
+        assert_eq!(average(&[]), None);
     }
 }
