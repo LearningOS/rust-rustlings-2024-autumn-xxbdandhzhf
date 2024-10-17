798cb2b1f239ba91f6d206fed6776cccdca46899
xxbd <lisa20110815@163.com>
2024-10-17 11:46:44 +0800

    solve traits2

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,23 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::vec::Vec;
 
+// Define the AppendBar trait
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+// Define a new struct that wraps a Vec<String> and implements the AppendBar trait
+struct Foo(Vec<String>);
+
+impl AppendBar for Foo {
+    fn append_bar(self) -> Self {
+        let mut inner = self.0;
+        inner.push(String::from("Bar"));
+        Foo(inner)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,8 +32,8 @@ mod tests {
 
     #[test]
     fn is_vec_pop_eq_bar() {
-        let mut foo = vec![String::from("Foo")].append_bar();
-        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
-        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+        let mut foo = Foo(vec![String::from("Foo")]).append_bar();
+        assert_eq!(foo.0.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.0.pop().unwrap(), String::from("Foo"));
     }
 }
