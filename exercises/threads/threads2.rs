9c9f8b9ad2a378a506a33caa7d825713d1dcd4e2
xxbd <lisa20110815@163.com>
2024-10-17 13:43:26 +0800

    solve threads2

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -1,39 +1,42 @@
-// threads2.rs
-//
-// Building on the last exercise, we want all of the threads to complete their
-// work but this time the spawned threads need to be in charge of updating a
-// shared value: JobStatus.jobs_completed
-//
-// Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 struct JobStatus {
     jobs_completed: u32,
+    // 添加一个互斥锁来保护jobs_completed的访问
+    lock: Mutex<u32>,
+}
+
+impl JobStatus {
+    fn new() -> Self {
+        JobStatus {
+            jobs_completed: 0,
+            lock: Mutex::new(0),
+        }
+    }
+
+    fn increment(&self) {
+        let mut guard = self.lock.lock().unwrap();
+        *guard += 1;
+    }
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(JobStatus::new());
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            // Update the shared value
+            status_shared.increment();
         });
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
-        // anything interesting in the output? Do you have to 'join' on all the
-        // handles?
-        println!("jobs completed {}", ???);
     }
+    // Print the value of JobStatus.jobs_completed after all threads have completed
+    println!("jobs completed {}", status.lock.lock().unwrap().clone());
 }
