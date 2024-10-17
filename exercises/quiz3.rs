7a924b5a6fd424df8ed9a5bac64363c2288336c4
xxbd <lisa20110815@163.com>
2024-10-17 12:04:38 +0800

    solve quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -1,33 +1,22 @@
-// quiz3.rs
-//
-// This quiz tests:
-// - Generics
-// - Traits
-//
-// An imaginary magical school has a new report card generation system written
-// in Rust! Currently the system only supports creating report cards where the
-// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
-// school also issues alphabetical grades (A+ -> F-) and needs to be able to
-// print both types of report card!
-//
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to
-// "A+" to show that your changes allow alphabetical grades.
-//
-// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+pub enum Grade {
+    Numerical(f32),
+    Alphabetical(String),
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        match &self.grade {
+            Grade::Numerical(num) => format!("{} ({}) - achieved a grade of {}",
+                                             &self.student_name, &self.student_age, num),
+            Grade::Alphabetical(alpha) => format!("{} ({}) - achieved a grade of {}",
+                                                  &self.student_name, &self.student_age, alpha),
+        }
     }
 }
 
@@ -38,7 +27,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numerical(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,9 +39,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alphabetical("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
